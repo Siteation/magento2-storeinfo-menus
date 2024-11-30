04f5937d5baca0f29cbf39557532ffef4ccf4448
grimlink <sean.grimlink@gmail.com>
2024-11-30 16:21:08 +0100

    ADD: drag option

--- Block/Adminhtml/Form/Field/Menu.php
+++ Block/Adminhtml/Form/Field/Menu.php
@@ -9,6 +9,7 @@
 namespace Siteation\StoreInfoMenus\Block\Adminhtml\Form\Field;
 
 use Magento\Config\Block\System\Config\Form\Field\FieldArray\AbstractFieldArray;
+use Magento\Framework\Data\Form\Element\AbstractElement;
 
 class Menu extends AbstractFieldArray
 {
@@ -26,5 +27,32 @@ class Menu extends AbstractFieldArray
 
         $this->_addAfter = false;
         $this->_addButtonLabel = __('Add');
+    }
+
+    protected function _getElementHtml(AbstractElement $element)
+    {
+        $id = $element['html_id'];
+        $html = parent::_getElementHtml($element);
+
+        $script = "<script>
+            document.addEventListener('DOMContentLoaded', function(event) {
+                require(['jquery', 'Magento_Theme/js/sortable'], function ($) {
+                    $('#" . $id . "').sortable({
+                        containment: 'parent',
+                        items: 'tbody tr',
+                        tolerance: 'pointer'
+                    });
+
+                    $('#" . $id . " thead tr').prepend('<th></th>');
+                    $('#" . $id . " tfoot tr').prepend('<th></th>');
+
+                    $('#" . $id . " tbody tr').prepend('<td><span class=\"draggable-handle\" style=\"display: flex; justify-content: center; align-items: center; margin-top: 0.5rem\"></span></td>');
+                });
+            });
+        </script>";
+
+        $html .= $script;
+
+        return $html;
     }
 }
